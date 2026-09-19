33246eacf66592ab56965b3a7b6d140fffb89cb8
MordovskoyKirill <mordak2006@gmail.com>
2026-09-19 17:55:43 +0900

    practice1

---
+++ Practice1/zadanie1.cs
@@ -0,0 +1,13 @@
+﻿/*
+Console.Write("Введите длину: ");
+double a = double.Parse(Console.ReadLine());
+
+Console.Write("Введите ширину: ");
+double b = double.Parse(Console.ReadLine());
+
+double S = a * b;
+double P = 2 * (a + b);
+
+Console.WriteLine($"Площадь: {S}");
+Console.WriteLine($"Периметр: {P}");
+*/
---
+++ Practice1/zadanie4.cs
@@ -0,0 +1,15 @@
+/*
+for (int student = 1; student <= 3; student++)
+{
+    Console.WriteLine($"\nСтудент {student}:");
+    double sum = 0;
+
+    for (int grade = 1; grade <= 5; grade++)
+    {
+        Console.Write($"Оценка {grade}: ");
+        sum += double.Parse(Console.ReadLine());
+    }
+
+    Console.WriteLine($"Средний балл: {sum / 5:F2}");
+}
+*/
